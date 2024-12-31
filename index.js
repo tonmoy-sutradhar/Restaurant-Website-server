1b0de13176f89a69a8f5c29a48bfd91e265b6f62
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-12-31 17:51:31 +0600

    Post the menu item in db.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,6 +7,42 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// -----------------------------------------MongoDB Connection--------------------------------------------------
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cjt8m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const menuCollection = client.db("Restaurant").collection("menu");
+
+    // Restaurant item add
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// -----------------------------------------MongoDB Connection--------------------------------------------------
 
 app.get("/", (req, res) => {
   res.send("Restaurant-Website-server is running");
